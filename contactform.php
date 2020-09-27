9b1ef50b7db3396b4247c6fbdedfe531d135a9e2
ALAN MATHIASEN <alan.mathiasen@outlook.com>
2020-09-27 11:53:34 -0300

    added validation contact form

--- contactform.php
+++ contactform.php
@@ -1,22 +1,38 @@
 
+
 <?php
-if (isset($_POST['submit'])){
-    
+    $message_sent = false;
+    if (isset($_POST['submit']) && $_POST['email'] != ''){
+     
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            //submit the form
+            $name = $_POST['name'];
+            $mailFrom = $_POST['email'];
+            $subject = $_POST['subject'];
+            $text = $_POST['text'];
 
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $subject = $_POST['subject'];
-    $text = $_POST['text'];
+            $mailTo = "alan.mathiasen@outlook.com";
+            $headers = "From: " . $mailFrom;
+            $txt = "Recibiste un mensaje de " . $name . ". \n\n" . $text;
 
-    $mailTo = "alan.mathiasen@outlook.com";
-    $headers = "From: " . $mailFrom;
-    $txt = "Recibiste un mensaje de " . $name . ". \n\n" . $text;
+            mail($mailTo, $subject, $txt, $headers);
+            $message_sent = true;
+            
 
-    mail($mailTo, $subject, $txt, $headers);
-    header('Refresh: 3; URL=index.html');
-    
-    
-}
+        }                 
+    }
 ?>
 
+<?php
+    if($message_sent):   
+?>
 Mensaje enviado correctamente! Te estamos redireccionando.
+<?php
+    else:   
+?>
+No se pudo enviar el mensaje, email incorrecto.
+
+<?php
+    endif;
+    header('Refresh: 3; URL=contacto.html');
+?> 
